7ec969d1668edab8c2c1bbb70f997e2f902438d5
szafran98 <szafran1308@gmail.com>
2020-05-06 13:51:36 +0200

    Initial commit

---
+++ api.py
@@ -0,0 +1,8 @@
+from pokemontcgsdk import Card
+from pokemontcgsdk import Set
+from pokemontcgsdk import Type
+from pokemontcgsdk import Supertype
+from pokemontcgsdk import Subtype
+
+
+'https://api.pokemontcg.io/v1/cards?setCode=ex6'
